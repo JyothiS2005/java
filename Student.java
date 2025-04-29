9d4d296f5ddcc43b6b5e3c09d3d8caf014bcdaed
Jyothi2005 <JyothiHudali05@gmail.com>
2025-04-29 16:44:00 +0530

    save my file to main branch

--- Student.java
+++ Student.java
@@ -7,7 +7,7 @@ class Study{
         this.name=n;
         this.age=a;
         this.dob=dob;
-
+    
 
     }
     void display(){
@@ -16,24 +16,10 @@ class Study{
         System.out.println("dob:" +dob);
     }
 }
-class Course{
-    void show(){
-        System.out.println("maths:021");
-        System.out.println("BDA:022");
-        System.out.println("Devops:023");
-    }
-
-}
-
 public class Student{
-    puSystem.out.println("Creating a new Study object..."); 
-Study S1=new Study("alice",19,"06-07-2005"); 
-Course c1=new Course();
+   public static void main (String[] args){
+    Study s1=new Study("alice" ,19,"06-07-2005");
+    s1.display();
 
-System.out.println("Study object created successfully!"); 
-S1.display();blic static void main(String[] args){
-        Study S1=new Study("alice",19,"06-07-2005");
-        S1.display();
-        c1.show();
+   }
     }
-}
