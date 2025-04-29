06e2d7fc6fef0a384017611e333155770e073962
Jyothi2005 <JyothiHudali05@gmail.com>
2025-04-29 16:10:13 +0530

    save it master

--- Student.java
+++ Student.java
@@ -8,6 +8,7 @@ class Study{
         this.age=a;
         this.dob=dob;
 
+
     }
     void display(){
         System.out.println("Name:" +name);
@@ -15,13 +16,24 @@ class Study{
         System.out.println("dob:" +dob);
     }
 }
+class Course{
+    void show(){
+        System.out.println("maths:021");
+        System.out.println("BDA:022");
+        System.out.println("Devops:023");
+    }
+
+}
 
 public class Student{
     puSystem.out.println("Creating a new Study object..."); 
 Study S1=new Study("alice",19,"06-07-2005"); 
+Course c1=new Course();
+
 System.out.println("Study object created successfully!"); 
 S1.display();blic static void main(String[] args){
         Study S1=new Study("alice",19,"06-07-2005");
         S1.display();
+        c1.show();
     }
 }
